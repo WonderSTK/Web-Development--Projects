466ffc75c610b991ce9bbcb6baaf37c9259eefdf
WonderSTK <mehulparmar9694@gmail.com>
2023-01-21 13:52:00 +0530

    all file modified and added

---
+++ React_project/react-complete-guide/src/App.js
@@ -0,0 +1,12 @@
+import expenceItem from './components/expenceItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <expenceItem></expenceItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ React_project/react-complete-guide/src/components/expenceItem.js
@@ -0,0 +1,17 @@
+import './expenceItem.css';
+
+function expenceItem() {
+    return(
+        <div className="">
+            <div>March 28th 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$256.26</div>
+            </div>
+        
+
+        </div>
+    );
+}
+
+export default expenceItem;
